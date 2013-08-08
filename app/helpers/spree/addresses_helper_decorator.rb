d298560551039c50e4c86d9d4847e6f18b370424
Kyle Macey <kyle@wholelattelove.com>
2013-08-08 16:52:05 -0400

    wrap up form fields

--- app/helpers/spree/addresses_helper_decorator.rb
+++ app/helpers/spree/addresses_helper_decorator.rb
@@ -6,14 +6,16 @@ module Spree
 	      if handler
 	        handler.call
 	      else
+	      	html = ""
 	        is_required = Spree::Address.required_fields.include?(method)
 	        separator = is_required ? '<span class="req">*</span><br />' : '<br />'
-	        content_tag :div, class: :span3 do
-	        	form.label(method) + separator.html_safe +
+	        html += content_tag :div, class: :span3 do
+	        	form.label(method) + separator.html_safe 
 	        end
-	        content_tag :div, class: :span9 do
+	        html += content_tag :div, class: :span9 do
 	        	form.text_field(method, :class => is_required ? 'required' : nil)
 	        end
+	        html.html_safe
 	      end
 	    end
 	  end
@@ -22,6 +24,7 @@ module Spree
 	    country ||= Spree::Country.find(Spree::Config[:default_country_id])
 	    have_states = !country.states.empty?
 	    state_elements = [
+	    	"<div class='span9'>",
 	      form.collection_select(:state_id, country.states.order(:name),
 	                            :id, :name,
 	                            {:include_blank => true},
@@ -29,12 +32,18 @@ module Spree
 	                            :disabled => !have_states}) +
 	      form.text_field(:state_name,
 	                      :class => !have_states ? "required" : "hidden",
-	                      :disabled => have_states)
+	                      :disabled => have_states),
+	      "</div>"
 	      ].join.gsub('"', "'").gsub("\n", "")
-
-	    form.label(:state, t(:state)) + '<span class="req">*</span><br />'.html_safe +
-	      content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
-	      javascript_tag("document.write(\"#{state_elements.html_safe}\");")
+	      content_tag :div, class: "row-fluid" do
+		      html = ""
+		    	html += content_tag :div, class: [:span3, :statecon] do
+		    		form.label(:state, t(:state)) + '<span class="req">*</span><br />'.html_safe
+		    	end
+		      html += content_tag(:noscript, form.text_field(:state_name, :class => 'required span9')) 	
+		      html += javascript_tag("document.write(\"#{state_elements.html_safe}\");")
+		      html.html_safe
+		    end
 	  end
   end
 end

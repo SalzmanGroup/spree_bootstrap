981a4b369b7b191d62d42430e38804f01dfd63d1
Kyle Macey <kyle@wholelattelove.com>
2013-08-08 15:49:39 -0400

    wrapp address fields

--- app/helpers/spree/addresses_helper_decorator.rb
+++ app/helpers/spree/addresses_helper_decorator.rb
@@ -2,14 +2,18 @@ module Spree
   AddressesHelper.class_eval do
   	def address_field(form, method, id_prefix = "b", &handler)
 	    id_prefix = id_prefix == 'bill_address' ? 'b' : 's'
-	    content_tag :p, :id => [id_prefix, method].join, :class => "field" do
+	    content_tag :div, :id => [id_prefix, method].join, :class => "field row-fluid" do
 	      if handler
 	        handler.call
 	      else
 	        is_required = Spree::Address.required_fields.include?(method)
 	        separator = is_required ? '<span class="req">*</span><br />' : '<br />'
-	        form.label(method) + separator.html_safe +
-	        form.text_field(method, :class => is_required ? 'required' : nil)
+	        content_tag :div, class: :span3 do
+	        	form.label(method) + separator.html_safe +
+	        end
+	        content_tag :div, class: :span9 do
+	        	form.text_field(method, :class => is_required ? 'required' : nil)
+	        end
 	      end
 	    end
 	  end
